41bf1f6f24fe03cc3a57863ed3253a24df0ec3ca
abrarhasinkml <ahasin12@gmail.com>
2021-11-06 04:10:06 +0100

    added asyncio and setup backend

--- cpc_api.py
+++ cpc_api.py
@@ -2,7 +2,7 @@ from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import sys
-
+import asyncio
 sys.path.insert(1, '.\secrets')
 
 from secrets import *
@@ -16,11 +16,22 @@ headers = {
 session = Session()
 session.headers.update(headers)
 
-try:
-    for crypt in crypto:
-        response = session.get(quotes_url+'?symbol='+crypt)
-        data = list(json.loads(response.text)['data'].values())[0]['quote']
-        quote = list(data.values())[0]
-        print(quote['price'])
-except(ConnectionError, Timeout, TooManyRedirects) as error:
-    print(error)
+async def get_latest_quote():
+    try:
+        quote=[]
+        for crypt in crypto:
+            response = session.get(quotes_url+'?symbol='+crypt)
+            crypt_key=list(json.loads(response.text)['data'].keys())[0]
+            data = list(json.loads(response.text)['data'].values())[0]['quote']
+            old_key=list(data.keys())
+            data[crypt_key]=data.pop(old_key[0])
+            quote.append(data)
+        current_quote=json.dumps(quote)  
+        return current_quote
+    except(ConnectionError, Timeout, TooManyRedirects) as error:
+        print(error)
+
+loop = asyncio.get_event_loop()
+response = loop.run_until_complete(get_latest_quote())
+
+    
